64a453fceaeb297073db39fd2daa527fa563f8ba
juli3p <julievicart13@gmail.com>
2019-04-19 11:56:45 +0200

    Exercices04-JQuery

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('#green').click(function(){
+    $('#text').css('color', 'green');
+  });
+});
+$(document).ready(function(){
+  $('#red').click(function(){
+    $('#text').css('color', 'red');
+  });
+});
+$(document).ready(function(){
+  $('#blue').click(function(){
+    $('#text').css('color', 'blue');
+  });
+});
